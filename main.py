0149336f03c1929528954f96d2416def49c93a18
luiz <luiz>
2022-11-01 09:20:21 -0300

    add get and post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from socket import J1939_NLA_BYTES_ACKED
+from tkinter.messagebox import RETRY
+from flask import Flask, render_template, request, jsonify
 import os
 
 app = Flask(__name__, template_folder='templates')
@@ -7,6 +9,13 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        return jsonify(request.form)
+    return jsonify({"messagem": "Utilize o formulário"})
 
 
 if __name__ == '__main__':
