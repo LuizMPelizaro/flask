57a24841c6792bb4992b2d726a90323f5559c557
luiz <luiz>
2022-11-01 09:44:00 -0300

    add neural

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from socket import J1939_NLA_BYTES_ACKED
 from tkinter.messagebox import RETRY
 from flask import Flask, render_template, request, jsonify
 import os
+import predictions
 
 app = Flask(__name__, template_folder='templates')
 
@@ -14,7 +15,16 @@ def index():
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
-        return jsonify(request.form)
+        dados = [
+            float(request.form['comp_sep']),
+            float(request.form['larg_sep']),
+            float(request.form['comp_petala']),
+            float(request.form['larg_petala'])
+        ]
+
+        y_pred = predictions.predict(dados)
+
+        return jsonify({"Resultado": y_pred})
     elif request.method == 'GET':
         return jsonify({"messagem": "Utilize o formulário"})
 
